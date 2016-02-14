7c9a8ef2c6962ff54c4208a6434c80e56027bc02
Brett Johnson <brettjohnson@iMac.local>
2016-02-14 15:59:56 -0800

    Styles work

--- js/main.js
+++ js/main.js
@@ -6,13 +6,21 @@ $('.thumbnails').magnificPopup({
     gallery: {
         enabled: true
     },
-    mainClass: 'mfp-with-zoom',
+
+//    mainClass: 'mfp-with-zoom',
     zoom: {
         enabled: true,
-        duration: 200,
-        easing: 'ease-out',
+        duration: 300,
+        easing: 'cubic-bezier(0.165, 0.84, 0.44, 1)',
         opener: function(openerElement) {
             return openerElement.is('img') ? openerElement : openerElement.find('img');
         }
-    }
+    },
+    
+    // Delay in milliseconds before popup is removed
+  removalDelay: 300,
+
+  // Class that is added to popup wrapper and background
+  // make it unique to apply your CSS animations just to this exact popup
+  mainClass: 'mfp-fade'
 });
